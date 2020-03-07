243f554ad8b3a68e5b4f44577a37286e62817b6d
Apple <apple@Apples-MBP.true.true>
2020-03-08 00:00:17 +0700

    Update gcloud deploy config

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -856,7 +856,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/apple/Projects/www/material-dashboard-angular2/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/apple/Projects/www/modSmart/src/main.ts */"./src/main.ts");
 
 
 /***/ })
